d3d89a85a332129049847c60bfc92b39f5a67911
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-08-27 11:11:09 -0500

    chore: add screenshots for debugging CI failures

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ async function main() {
   const context = await browser.newContext();
   const initialPage = await context.newPage();
 
-  const maxAttempts = 3;
+  const maxAttempts = 1;
   let attempts = 0;
 
   while (attempts < maxAttempts) {
@@ -28,27 +28,34 @@ async function main() {
     try {
       console.log('Navigating to library website');
       await initialPage.goto('https://www.olathelibrary.org/online-resources/online-entertainment#enewspapers', { timeout: 300_000 });
-    
+      await initialPage.screenshot({ path: 'screenshots/1.png' });
+
       console.log('Clicking on New York Times');
       const libLoginPagePromise = context.waitForEvent('page');
       await initialPage.getByRole('link', { name: /new york times/i }).click();
       const loginPage = await libLoginPagePromise;
-    
+
       console.log('Logging in to library');
+      await loginPage.screenshot({ path: 'screenshots/2.png' });
       await loginPage.getByLabel('Username or Barcode:').pressSequentially(env.LIB_USERNAME, { delay: 150 });
       await loginPage.getByLabel('PIN/Password :').pressSequentially(env.LIB_PASSWORD, { delay: 150 });
       await loginPage.getByRole('button', { name: /log in/i }).click();
       await loginPage.waitForURL(/nytimes/i);
-    
+      await loginPage.waitForLoadState('networkidle');
+
       console.log('Logging in to NYT');
+      await loginPage.screenshot({ path: 'screenshots/3.png' });
       await loginPage.getByTestId('login-lnk').click();
       await loginPage.waitForURL(/myaccount\.nytimes/i);
+      await loginPage.waitForLoadState('networkidle');
+      await loginPage.screenshot({ path: 'screenshots/4.png' });
+
       await loginPage.getByLabel('Email Address').pressSequentially(env.NY_USERNAME, { delay: 150 });
       await loginPage.getByTestId('submit-email').click();
       await loginPage.getByLabel('Password', { exact: true }).pressSequentially(env.NY_PASSWORD, { delay: 150 });
       await loginPage.getByTestId('login-button').click();
-      await loginPage.waitForLoadState('networkidle');
-    
+      await loginPage.screenshot({ path: 'screenshots/5.png' });
+
       const confirmationText = loginPage.getByText(/set a calendar reminder to renew/i);
       const confirmationTextVisible = await confirmationText.isVisible();
     
